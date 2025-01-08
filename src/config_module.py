c63423e1633f3d21ea10197fa1144f8826fccf73
Artur Koltyrin <koltyrinartur@gmail.com>
2025-01-08 16:40:13 +0500

    создан databaseini и переписана функция main и создан user interection

--- src/config_module.py
+++ src/config_module.py
@@ -1,7 +1,7 @@
 from configparser import ConfigParser
 
 
-def config(filename="../database.ini", section="postgresql"):
+def config(filename="../database.ini", section="postgres"):
     parser = ConfigParser()
     parser.read(filename)
     db = {}
